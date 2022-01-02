84ac6e16330e2cb86e7cb32e22a1b04893332c22
psbrunosouza <psbrunosouza@gmail.com>
2022-01-02 10:57:41 -0300

    feat(components/arbol-link): create custom link

---
+++ src/app/pages/@theme/arbol-link/arbol-link.component.ts
@@ -0,0 +1,17 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'ab-link',
+  templateUrl: './arbol-link.component.html',
+  styleUrls: ['./arbol-link.component.scss']
+})
+export class ArbolLinkComponent implements OnInit {
+
+  @Input() public to: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
